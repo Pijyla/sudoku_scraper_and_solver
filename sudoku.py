0bf4c47fa9eff2c0fdcbe6b77964fb9626498eb9
Pijyla <91634257+Pijyla@users.noreply.github.com>
2021-11-27 21:24:15 +0100

    sudoku.py
    Populate game puzzle with numbers, freeze that numbers and make changes to GUI

--- sudoku.py
+++ sudoku.py
@@ -1,36 +1,36 @@
 import tkinter as tk
 from tkinter import *
-from tkinter.font import BOLD
 import requests
 from bs4 import BeautifulSoup
 
 LIGHT_BLUE = "#CCEDFF"
 LIGHT_GRAY = "#F5F5F5"
+MEDIUM_GRAY ="#808080"
 GREEN = "#00CC66"
 YELLOW = "#FFFF99"
 RED = "#FF6666"
 BLUE = "#66B2FF"
+DARK_BLUE = "#000066"
 PETROL_GREEN = "#66FFB2"
 
 cells = {}
-game_board = {}
 
 class Sudoku:
     def __init__(self):
         self.window = tk.Tk()
-        self.window.geometry("370x610")
+        self.window.geometry("450x752")
         self.window.resizable(0,0)
         self.window.title('S U D O K U')
         self.window.configure(background="gray")
-        self.grid9x9 = self.grid9x9()
-        self.puzzle = self.get_Puzzle("1")
+        self.puzzle = self.get_and_populate_puzzle("1")
         self.number_buttons = self.create_number_buttons()
         self.level_nuttons = self.create_level_buttons()
         self.play_buttons = self.create_play_buttons () 
         self.grid = []
+        self.board = []
 
     # Web scraping Sudoku board initializing numbers from https://nine.websudoku.com:
-    def get_Puzzle(self, difficulty):
+    def get_and_populate_puzzle(self, difficulty):
         # Difficulty passed in as string with one digit. 1-4
         html_doc = requests.get("https://nine.websudoku.com/?level={}".format(difficulty)).content
         soup = BeautifulSoup(html_doc, 'html.parser')
@@ -43,51 +43,60 @@ class Sudoku:
         data = []
         for cid in ids:
             data.append(soup.find('input', id=cid))
+        #print(data)
         board = [[0 for x in range (9)] for x in range(9)]
         for index, cell in enumerate(data):
             try:
                 board[index//9][index%9] = int(cell['value'])
+                #print(board[index//9][index%9]) # Numbers from dudoku starting game
             except:
                 pass
+        #print (board)
+        # CREATE NESTED LIST (9 LISTS WITH 9 ELEMENTS):
         self.grid = board
-        #print(self.grid)
-        for i, a in enumerate (board):
-            game_board [i] = a
-            print (a)
+        #print(board)
 
-    def grid9x9 (self):
-        # Validate inputs for entry fields, only numbers are allowed:
-        def ValidateNumber(P):
-            out = (P.isdigit() or P == "") and len(P) < 2
-            return out
-        reg = self.window.register(ValidateNumber)
         # Draw grid 9x9 of 81 entry widgets:   
-        for i in range (9):
-            for j in range (9):
+        for column in range (9):
+            for row in range (9):
                 color = LIGHT_BLUE
-                global e
-                e = Entry(self.window, width = 3, bg=color, justify="center", validate="key", validatecommand=(reg, "%P"))
-                e.grid(row=i+1, column=j+1, sticky="nsew", padx=1, pady=1,ipady=5)
-                cells[(i+1, j+1)] = e
+                def ValidateNumber(P):
+                    out = (P.isdigit() or P == "") and len(P) < 2
+                    return out
+                reg = self.window.register(ValidateNumber)
+                e = tk.IntVar()
+                if board[row][column] == 0:
+                    e.set("")
+                else:
+                    e.set(board[row][column])
+                entry = Entry(self.window, textvariable=e, width = 3, bg=color, justify="center", font=('Arial', 20, 'bold'), validate="key", validatecommand=(reg, "%P")) # STATE = DISABLE (for restriction to change content)
+                entry.grid(row=row+1, column=column+1, sticky=NSEW, padx=1, pady=1, ipady=5)
+                 #if board[row][column] == 0:
+                    #entry.config(state='normal')
+                #else:
+                    #entry.config(state='readonly')
+                entry.config(state='normal' if board[row][column] == 0 else 'readonly') # Using ARGS to write it shorter
+                entry.config(readonlybackground=LIGHT_GRAY, foreground=DARK_BLUE if board[row][column] != 0 else LIGHT_BLUE and 'black')
+                cells[(row+1, column+1)] = e
 
     def create_number_buttons(self):
         for i in range (1,10,10):
             for j in range (1,10):
-                btn = Button(self.window, width=2, height=3, text=j, highlightbackground=LIGHT_GRAY, justify="center")
+                btn = Button(self.window, width=2, height=3, text=j, highlightcolor=MEDIUM_GRAY, justify="center", font=('Arial', 15, 'bold'))
                 btn.grid(row=10, column=j-1+1, sticky="nsew", padx=1, pady=1, ipadx=5)
 
     def create_level_buttons(self):
-        level_easy = Button(self.window, width=6, height=3, text="EASY", highlightbackground=GREEN, justify="center")
+        level_easy = Button(self.window, width=6, height=3, text="EASY", highlightbackground=GREEN, justify="center", font=('Arial', 20, 'bold'))
         level_easy.grid(row=13, column=1, sticky="nsew", padx=1, pady=1, ipadx=5, columnspan=3)               
-        level_medium = Button(self.window, width=6, height=3, text="MEDIUM", highlightbackground=YELLOW, justify="center")
+        level_medium = Button(self.window, width=6, height=3, text="MEDIUM", highlightbackground=YELLOW, justify="center", font=('Arial', 20, 'bold'))
         level_medium.grid(row=13, column=4, sticky="nsew", padx=1, pady=1, ipadx=5, columnspan=3) 
-        level_hard = Button(self.window, width=6, height=3, text="HARD", highlightbackground=RED, justify="center")
+        level_hard = Button(self.window, width=6, height=3, text="HARD", highlightbackground=RED, justify="center", font=('Arial', 20, 'bold'))
         level_hard.grid(row=13, column=7, sticky="nsew", padx=1, pady=1, ipadx=5, columnspan=3) 
 
     def create_play_buttons (self):
-        level_easy = Button(self.window, width=9, height=4, text="CHECK", highlightbackground=BLUE, justify="center")
+        level_easy = Button(self.window, width=9, height=4, text="CHECK", highlightbackground=BLUE, justify="center", font=('Arial', 20, 'bold'))
         level_easy.grid(row=14, column=1, sticky="nsew", padx=1, pady=1, ipadx=5, columnspan=9)               
-        level_medium = Button(self.window, width=9, height=4, text="SOLVE", highlightbackground=PETROL_GREEN, justify="center")
+        level_medium = Button(self.window, width=9, height=4, text="SOLVE", highlightbackground=PETROL_GREEN, justify="center", font=('Arial', 20, 'bold'))
         level_medium.grid(row=15, column=1, sticky="nsew", padx=1, pady=1, ipadx=5, columnspan=9) 
 
     def run(self):
